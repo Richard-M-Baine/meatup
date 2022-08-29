c52512e40d037cae6cfcccbdb518b8c9fb054598
richard baine <efdeliveryservice@gmail.com>
2022-08-29 19:03:11 -0400

    eventimage model/migration done

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -42,7 +42,7 @@ module.exports = (sequelize, DataTypes) => {
     }
 
     static associate(models) {
-      User.hasMany(models.Attendee, {foreignKey: 'userId'})
+      User.hasMany(models.Attendance, {foreignKey: 'userId'})
     }
   }
   User.init({
