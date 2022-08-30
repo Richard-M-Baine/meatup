4c1f6848adfabf101774a6ca5ce22cb282afedff
richard baine <efdeliveryservice@gmail.com>
2022-08-30 19:47:35 -0400

    first end point works

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -11,24 +11,36 @@ const router = express.Router();
 
 
 router.get('/', async (req,res,next) => {
-
-    
+let returnobject = {}
     const groups = await Group.findAll({
         include: [
             {
                 model: GroupImage,
                 as: 'previewImage',
                 attributes: ['url'],
-                limit: 1
-               
+                limit: 1            
             },
            
         ],
     })
-    
+const members = await Membership.findAll()
+   
+let scaryarray = []
 
+for (let i = 0; i < groups.length; i ++){
+let group = groups[i]
+let {id:value1,organizerId:value2,name:value3,about:value4,type:value5,private:value6,city:value7,state:value8,createdAt:value9,updatedAt:value10,previewImage:value11} = group
+let count = 0
+for (let i = 0; i < members.length; i ++){
+    let member = members[i]
+    if (member.groupId === value1){count = count + 1}
+}
+let numMembers = 'numMembers'
+let object = {id:value1,organizerId:value2,name:value3,about:value4,type:value5,private:value6,city:value7,state:value8,createdAt:value9,updatedAt:value10,numMembers:count,previewImage:value11}
+scaryarray.push(object)
+}  
 
-
+res.json(scaryarray)
 })
 
 module.exports = router;
