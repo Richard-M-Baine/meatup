18761fd6de1cdb7973e0e6e3751efd8b7d12ab76
richard baine <efdeliveryservice@gmail.com>
2022-08-30 16:01:04 -0400

    added signup validation halfway done for testing purposes

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -3,11 +3,34 @@ const express = require('express');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const validateSignup = [
+  check('firstName')
+    .exists({ checkFalsy: true })
+    .isLength({min: 1})
+    .withMessage('First Name is required.'),
+  check('lastName')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 2 })
+    .withMessage('Last Name is required.'),
+  check('email')
+    .isEmail()
+    .withMessage('Invalid email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 2 })
+    .withMessage('you could make a better password.'),
+  handleValidationErrors
+];
+
 const router = express.Router();
 
 // Sign up
 router.post(
   '/',
+  validateSignup,
   async (req, res) => {
     const { firstName, lastName, email, password} = req.body;
     const user = await User.signup({ firstName, lastName,email,password});
