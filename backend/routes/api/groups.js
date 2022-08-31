050adc2524a37e41a07b0046fbd6097a6c6dbf9a
richard baine <efdeliveryservice@gmail.com>
2022-08-30 21:42:05 -0400

    last commit half done with second group current user

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -10,26 +10,60 @@ const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
 
-router.get(
-    '/current',
-    requireAuth,
-    async (req, res, next) => {
-      const groups = await Group.findAll({
+router.get('/current',requireAuth,async (req, res, next) => {
+      const groupBoss = await Group.findAll({
         where: {
           organizerId: req.user.id
         },
         include: [
           {
-            model: GroupImage, // returns an array. clarify during stand up how to properly do this query
+            model: GroupImage, // good except numberOf
             as: 'previewImage',
             attributes: ['url'],
             limit: 1
           },
         ],
       })
+      bossArray = []
+      const groupSoldaten = await Group.findAll({
+        include: [
+          {
+            model: GroupImage, // worse comes to worse do object destructure in am
+            as: 'previewImage',
+            attributes: ['url'],
+            limit: 1
+          },
+          {
+            model: Membership,
+            attributes: [],
+            where: {
+              userId: req.user.id
+            }
+          }
+        ]
+      })
 
-res.json({'Groups':groups})
-    })
+      const members = await Membership.findAll()
+
+
+      for (let i = 0; i < groupBoss.length; i ++){
+        let group = groupBoss[i]
+        let {id:value1,organizerId:value2,name:value3,about:value4,type:value5,private:value6,city:value7,state:value8,createdAt:value9,updatedAt:value10,previewImage:value11} = group
+        let count = 0
+        for (let i = 0; i < members.length; i ++){
+            let member = members[i]
+            if (member.groupId === value1){count = count + 1}
+        }
+        let numMembers = 'numMembers'
+        let object = {id:value1,organizerId:value2,name:value3,about:value4,type:value5,private:value6,city:value7,state:value8,createdAt:value9,updatedAt:value10,numMembers:count,previewImage:value11}
+        bossArray.push(object)
+
+ 
+    }
+    res.json({'Groups':bossArray})
+    
+}
+)
 
 
 
