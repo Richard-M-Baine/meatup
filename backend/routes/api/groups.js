1913e86eb4c587e7c714dcbef64f45f0be01497e
richard baine <efdeliveryservice@gmail.com>
2022-08-31 14:02:34 -0400

    copy and paste validtion errors

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -9,6 +9,36 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
+//validation errors
+const validateGroup = [
+  check('name')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 5, max: 60 })
+      .withMessage('Name must be 60 characters or less'),
+  check('about')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 50 })
+      .withMessage('About must be 50 characters or more'),
+  check('type')
+      .exists({ checkFalsy: true })
+      .isIn(['Online', 'In person'])
+      .withMessage('Type must be Online or In person'),
+  check('private')
+      .exists()
+      .isBoolean()
+      .withMessage('Private must be a boolean'),
+  check('city')
+      .exists({ checkFalsy: true })
+      .withMessage('City is required'),
+  check('state')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 2, max: 2 })
+      .withMessage('State is required'),
+  handleValidationErrors
+]
+
+
+
 
 
 
