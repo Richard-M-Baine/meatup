43a6f1f454e2a700e8a98f28719ce08892983dbb
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-09-18 10:14:49 -0400

    can sign up a user now

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -18,6 +18,22 @@ export const restoreUser = () => async dispatch => {
   return response;
 };
 
+export const signup = (user) => async (dispatch) => {
+  const { firstName, lastName, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      firstName,
+      lastName,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
 const removeUser = () => {
   return {
     type: REMOVE_USER,
@@ -25,7 +41,7 @@ const removeUser = () => {
 };
 
 
-// Dan Purcell Thunk
+// Dan Purcell Thunk / login
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
   const response = await csrfFetch('/api/session', {
