5779d20a7a2278ca85ed848e92b019f77d57ce02
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-09-19 15:32:58 -0400

    restart due to tooth issues

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,35 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider as ReduxProvider } from 'react-redux';
 import './index.css';
 import App from './App';
-
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import configureStore from './store';
-
-import { restoreCSRF, csrfFetch } from './store/csrf';
-
-import * as sessionActions from './store/session';
-
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== 'production') {
-  restoreCSRF();
-
-  window.csrfFetch = csrfFetch;
+if (process.env.NODE_ENV !== "production") {
   window.store = store;
-  window.sessionActions = sessionActions;
 }
-
 
 function Root() {
   return (
-    <Provider store={store}>
+    <ReduxProvider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </Provider>
+    </ReduxProvider>
   );
 }
 
@@ -37,5 +26,5 @@ ReactDOM.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
