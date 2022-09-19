cb5a7eb6eb40915cb723aa7432ea9bc0eb50707d
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-09-19 18:16:53 -0400

    playing with front page deffered due to tooth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,23 @@
+// I am the Main APP
+// I live in the src folder
+import { Route, Switch } from "react-router-dom";
+import React from "react";
+
+import Splash from "./components/Splash";
+
 function App() {
   return (
+    <>
     <h1>Hello from App</h1>
+    <Switch>
+      <Route exact path='/'>
+      <Splash />
+      </Route>
+
+
+
+    </Switch>
+    </>
   );
 }
 
