be6a6690cdf92d890b961bc7c7b55465db7ffbd7
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-10-15 19:51:03 -0400

    added all choices etc for the events should avoid issues like last time

--- frontend/src/store/events.js
+++ frontend/src/store/events.js
@@ -8,6 +8,8 @@ const CREATE_EVENT = "events/create"
 const READ_EVENT = "events/details"
 const DELETE_EVENT = "events/delete"
 
+// action creators fewer spaces
+
 const getEventsAction = (payload) => {
     return {
         type: GET_EVENTS,
@@ -42,3 +44,56 @@ const deleteEventAction = (payload) => {
         payload
     }
 }
+
+// lost in the sauce of thunkville
+
+export const getEventsThunk = () => async dispatch => {
+    const response = await csrfFetch('/api/events')
+    const data = await response.json()
+
+    await dispatch(getEventsAction(data))
+    return data
+}
+
+const initialState = {}
+
+const eventsReducer = (state = initialState, action) => {
+    let newState = {}
+    switch (action.type) {
+        case GET_EVENTS: {
+            action.payload.Events.forEach(event => {
+                newState[event.id] = event
+            })
+            return newState
+        }
+        case GET_GROUPS_EVENTS: {
+            action.payload.Events.forEach(event => {
+                newState[event.id] = event
+            })
+            return newState
+        }
+        case CREATE_EVENT:{
+            newState = { ...state }
+            newState[action.payload.id] = action.payload
+            return newState
+        }
+        case READ_EVENT: {
+            newState = { ...state }
+            newState[action.payload.id] = { ...newState[action.payload.id], ...action.payload } // Gets the new data from the response and adds it to what exists
+            return newState
+        }
+        // case UPDATE_EVENT: {
+
+        // }
+        case DELETE_EVENT: {
+            newState = { ...state }
+            delete newState[action.payload]
+            return newState
+        }
+        default: {
+            return state
+        }
+    }
+}
+
+export default eventsReducer
