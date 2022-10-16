851b8a4c4338bff5fb476abf0f05dd8e4f653b46
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-10-15 21:08:22 -0400

    get all events works!

--- frontend/src/components/Events/ReadEvents/EventCard/index.js
+++ frontend/src/components/Events/ReadEvents/EventCard/index.js
@@ -4,8 +4,47 @@ import { NavLink } from "react-router-dom";
 import "./EventCard.css"
 
 function EventCard({ event }) {
+    console.log(event.name)
+
+    // getting all the stuff from the start date
+    const start = new Date(event.startDate)
+    const splitStart = start.toString().split(' ')
+        // getting all the stuff 
+    const day = splitStart[0]
+    const date = `${splitStart[1]} ${splitStart[2]}`
+    const militaryTime = start.getHours()
+    const minutess = start.getMinutes()
+    
+
+    // for stupid civilians and for practicing ternaries
+    const civilians = Number(militaryTime) > 12 ? Number(militaryTime) - 12 : militaryTime
+    const stupidCivilians = Number(militaryTime) > 12 ? 'PM' : 'AM'
+    const realMinutes = (minutess > 10) ? minutess : '0'.concat(minutess)
+    const people = (event.numAttending > 1 || event.numAttending === 0) ? 'persons' : 'person'
+
+
+    
+
     return (
-        <h1>you are terrible</h1>
+        <NavLink to={`/events/${event.id}/about`}>
+        <div className="imageDiv">
+            <img
+                        className='card-image'
+                        src={event.previewImage?.length > 0 ? event.previewImage : ""}
+                        hidden={event.previewImage?.length > 0 ? false : true}
+            />
+        </div>
+
+        <div className="second">
+            <h1>{event.name}</h1>
+            <h3>{`next meeting ${day} ${date} at ${civilians}:${realMinutes} ${stupidCivilians}`}</h3>
+            <h4>Hosted By {event.Group.name} from {event.Group.city} {event.Group.state}</h4>
+            <h4>There currently {event.numAttending} {people} attending!</h4>
+
+        </div>
+        
+
+        </NavLink>
     )
 }
 
