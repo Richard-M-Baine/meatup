56aac3539407b48ce335fc95f48415aee9e62299
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-10-17 16:23:30 -0400

    flavor text and finishing description

--- frontend/src/components/Events/DeleteEvent/index.js
+++ frontend/src/components/Events/DeleteEvent/index.js
@@ -30,8 +30,26 @@ const DeleteEvent = () => {
     
 
     return (
-        <h1>You are terrible</h1>
+        <div className='main-container'>
+                <div className='top-container_'>
+                    
+
+                    <div className='introtopDiv'>
+                        <h1 className='name'>{thisEvent.name} detail Showcase!</h1>
+                        <h3 className='location'> Proudly hosted at  {thisEvent.Venue.address}, {thisEvent.Venue.city}, {thisEvent.Venue.state}</h3>
+                        <h4>by {thisEvent.Group.name}</h4>
+                    </div>
+                    <div className='descriptionDiv'>
+                        <p>{thisEvent.description}</p>
+                        <p>there are currently {thisEvent.numAttending} people attending and only {thisEvent.capacity - thisEvent.numAttending} spots left!</p>
+                    </div>
+                    <div className='startDiv'>
+                        <p>We will start at {thisEvent.startDate}</p>
+                    </div>
+                </div>
+        </div>   
     )
+    
 
 }
 
