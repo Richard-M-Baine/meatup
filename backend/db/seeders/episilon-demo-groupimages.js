102f54381fb92fbe4e9e1ca0e074a0dee3972553
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-10-18 16:40:59 -0400

    added url seed files

--- backend/db/seeders/episilon-demo-groupimages.js
+++ backend/db/seeders/episilon-demo-groupimages.js
@@ -11,21 +11,21 @@ module.exports = {
       },
       {
         groupId:2,
-        url: "asdfsadfasdf",
-        preview:false,
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk-k_MzS6tLUeL2zyt6sx_f8Bcuew1ylYETQ&usqp=CAU",
+        preview:true,
       },
       {
         groupId:3,
         url:"https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.geekalerts.com%2Fu%2FLittle-Fat-Cthulhu-Chess-Set.jpg&imgrefurl=https%3A%2F%2Fwww.geekalerts.com%2Fcthulhu-custom-chess-set%2F&tbnid=agt6EqLwd2DEiM&vet=12ahUKEwjP4a2Oy-r6AhVQrXIEHcW2ALAQMygSegUIARDQAQ..i&docid=b-eVYw74-QgMdM&w=584&h=369&q=cthulhu%20chess&ved=2ahUKEwjP4a2Oy-r6AhVQrXIEHcW2ALAQMygSegUIARDQAQ",
-        preview:false
+        preview:true
       },
       {groupId:1,
-        url:'smilingGrouppicture4',
+        url:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.k00QJxLEBFw10AVg2XOU9AEsD5%26pid%3DApi&f=1&ipt=51cc1504cf8100cb50ffb4d4d8437131395f3c169764dbf08b69f6cbd2c3b17c&ipo=images",
       preview:false
       },
       {
         groupId:2,
-        url:'smilingGrouppicture5',
+        url:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.k00QJxLEBFw10AVg2XOU9AEsD5%26pid%3DApi&f=1&ipt=51cc1504cf8100cb50ffb4d4d8437131395f3c169764dbf08b69f6cbd2c3b17c&ipo=images",
         preview:false
       }
 
