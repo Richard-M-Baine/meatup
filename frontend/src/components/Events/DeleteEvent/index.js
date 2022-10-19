3c0680f9d9fc5799519c1c8da34ff0adf7c651ff
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-10-19 18:28:09 -0400

    event details looks pretty

--- frontend/src/components/Events/DeleteEvent/index.js
+++ frontend/src/components/Events/DeleteEvent/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import {useDispatch, useSelector} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useParams, useHistory } from "react-router-dom";
 import { Link } from 'react-router-dom';
 
@@ -11,119 +11,121 @@ import { deleteEvent } from '../../../store/events';
 
 const DeleteEvent = () => {
     const dispatch = useDispatch()
-    
-    const {eventId} = useParams()
+
+    const { eventId } = useParams()
     const history = useHistory();
 
     const thisEvent = useSelector((state) => state.events[eventId]);
     const user = useSelector(state => state.session.user);
-   
+
 
     //  moment of truth
 
     const isOwner = user?.id === thisEvent?.Group.organizerId
 
-     // get event stuff
-     useEffect(() => {
-        
-        
+    // get event stuff
+    useEffect(() => {
+
+
         dispatch(getOneEventThunk(eventId)).then(() => setLoaded(true))
-    },[dispatch, eventId])
+    }, [dispatch, eventId])
 
     const [loaded, setLoaded] = useState(false)
 
 
 
     const makeEventgoByeBye = e => {
-        console.log('i am here and this is the eventid ',eventId)
+        console.log('i am here and this is the eventid ', eventId)
         e.preventDefault()
         setLoaded(false)
         dispatch(deleteEvent(eventId)).then(() => history.push('/events/all'))
-     
-     
+
+
     }
-   
-   
-
-   
 
 
 
-    if (!thisEvent){
+
+
+
+
+    if (!thisEvent) {
         return null
     }
     // stuff with dates
 
     const start = new Date(thisEvent.startDate)
     const splitStart = start.toString().split(' ')
-        // getting all the stuff 
+    // getting all the stuff 
     const day = splitStart[0]
     const date = `${splitStart[1]} ${splitStart[2]}`
     const militaryTime = start.getHours()
     const minutess = start.getMinutes()
-    
+
     const civilians = Number(militaryTime) > 12 ? Number(militaryTime) - 12 : militaryTime
     const stupidCivilians = Number(militaryTime) > 12 ? 'PM' : 'AM'
     const realMinutes = (minutess > 10) ? minutess : '0'.concat(minutess)
     const people = (thisEvent.numAttending > 1 || thisEvent.numAttending === 0) ? 'persons' : 'person'
 
- 
-  const end = new Date(thisEvent.endDate)
-  const splitEnd = end.toString().split(' ')
-  const militarischZeit = end.getHours()
-  const minuten = end.getMinutes()
 
-  const endCivilians = Number(militarischZeit) > 12 ? Number(militarischZeit) - 12: militarischZeit
-  const idioten = Number(militarischZeit) > 12 ? 'PM' : 'AM'
-  const endMinutes = (minuten > 10) ? minuten : '0'.concat(minuten)
- 
+    const end = new Date(thisEvent.endDate)
+    const splitEnd = end.toString().split(' ')
+    const militarischZeit = end.getHours()
+    const minuten = end.getMinutes()
 
-// end stuff with dates
+    const endCivilians = Number(militarischZeit) > 12 ? Number(militarischZeit) - 12 : militarischZeit
+    const idioten = Number(militarischZeit) > 12 ? 'PM' : 'AM'
+    const endMinutes = (minuten > 10) ? minuten : '0'.concat(minuten)
+
+
+    // end stuff with dates
 
 
 
     return loaded && (
-        <div className='main-container'>
-                <div className='top-container_'>
-                    <div className='imageDiv'>
-                        <img id='EDphoto'src={thisEvent.previewImage?.length > 0 ? thisEvent.previewImage[0].url : ""}/>
-                    </div>
-                    
+        <div className='mainEDdetails'>
+            <div className='topEDcontainer'>
+                <div className='leftEDPhotos'>
+                    <img id='EDphoto' src={thisEvent.previewImage?.length > 0 ? thisEvent.previewImage[0].url : ""} />
+                </div>
 
-                    <div className='introtopDiv'>
+                <div className='theothersectionofEDtop'>
+                    <div className='introEDtopDiv'>
                         <h1 className='name'>{thisEvent.name}!</h1>
-                    <div className='EDaddressParentDiv'>
-                        <h3>We will meet at </h3>
-                        <h4 className='location'>{thisEvent.Venue ?  thisEvent.Venue.address : 'The organizer has not determined the location of the event yet'} {thisEvent.Venue ? thisEvent.Venue.city : ''} {thisEvent.Venue ? thisEvent.Venue.state: ''}</h4>
+                        <div className='EDaddressParentDiv'>
+                            <h3>We will meet at </h3>
+                            <h4 className='location'>{thisEvent.Venue ? thisEvent.Venue.address : 'The organizer has not determined the location of the event yet'} {thisEvent.Venue ? thisEvent.Venue.city : ''} {thisEvent.Venue ? thisEvent.Venue.state : ''}</h4>
                         </div>
                         <h4>Proudly hosted by {thisEvent.Group.name}</h4>
                     </div>
-                        <div className='descriptionDiv'>
+                    <div className='descriptionDiv'>
                         <p>{thisEvent.description}</p>
                         <p>there are currently {thisEvent.numAttending} {people} attending and only {thisEvent.capacity - thisEvent.numAttending} spots left!</p>
-                        </div>
-                    <div className='startDiv'>
-                    <h3>{`we will start at ${day} ${date} at ${civilians}:${realMinutes} ${stupidCivilians}`} {`and end at ${endCivilians}:${endMinutes} ${idioten}`}</h3>
-                        
                     </div>
-                    {isOwner && 
-                    <div className='flavorText'>
-                    <p>Hi Mr / Mrs. {user.lastName} if you wish to cancel this event please click below.</p>
-                    <p>Please be warned that there is no way to undo this process</p>
-                    </div>}
-
-                    {isOwner && 
-                    <button className='submitButton' onClick={makeEventgoByeBye}>Cancel {thisEvent.name}</button>}
-                    
-                    
+                    <div className='startDiv'>
+                        <h3>{`we will start at ${day} ${date} at ${civilians}:${realMinutes} ${stupidCivilians}`} {`and end at ${endCivilians}:${endMinutes} ${idioten}`}</h3>
+                    </div>
                 </div>
+            </div>
+            {isOwner &&
+                <div className='EDflavorText'>
+                    <p>Hi Mr / Mrs. {user.lastName} if you wish to cancel this event please click below.</p>
+                    <p id='thesecondbottomED'>Please be warned that there is no way to undo this process</p>
+                    <button className='submitEDButton' onClick={makeEventgoByeBye}>Cancel This Event</button>
+                </div>}
 
-              
 
-               
-        </div>   
+
+
+
+
+
+
+
+
+        </div>
     )
-    
+
 
 }
 
--- frontend/src/components/Groups/GroupDetails/index.js
+++ frontend/src/components/Groups/GroupDetails/index.js
@@ -5,34 +5,34 @@ import { useEffect, useState } from 'react';
 import * as sessionActions from '../../../store/session';
 
 import { useDispatch, useSelector } from 'react-redux';
-import { useHistory, useParams} from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 
 //css
 import './GroupDetails.css'
 
 // thunk
-import {getOneGroupThunk} from '../../../store/group'
+import { getOneGroupThunk } from '../../../store/group'
 
 
-export default function GroupDetails(){
+export default function GroupDetails() {
 
     const dispatch = useDispatch();
     const { groupId } = useParams();
-   const reduxstate = useSelector((state) => state.groups);
+    const reduxstate = useSelector((state) => state.groups);
     const singleGroup = reduxstate[groupId];
-   
-    
+
+
 
     const thisUser = useSelector(state => state.session.user);
 
     const [isLoaded, setIsLoaded] = useState(false)
-    
-    const history = useHistory();
-    
 
-    
+    const history = useHistory();
+
+
+
     const isOwner = thisUser?.id === singleGroup?.organizerId
-  
+
 
 
     useEffect(() => {
@@ -53,36 +53,36 @@ export default function GroupDetails(){
 
 
     return isLoaded && (
-        
-       <div className='mainGroupDetails'>
-                <div className='groupDetailsTop'>
-                    <div className='leftGroupDetailPhotos'>
-                    <img id='GDphoto'src={singleGroup.groupImages?.length > 0 ? singleGroup.groupImages[0].url : ""} />
-                
-                    </div>
 
-                    <div className='rightTopGroupDetails'>
-                        <h2 className='GDname'>{singleGroup.name}</h2>
-                        <h3 className='GDlocation'> {singleGroup.city}, {singleGroup.state}</h3>
-                        <div className='GDmembers-public'> {singleGroup.numMembers} {singleGroup.numMembers === 1 ? "member" : "members"} · {singleGroup.private ? "Private" : "Public"} group </div>
-                        <div className='GDorganizer'> Organized by {singleGroup.organizer.firstName}</div>
-                        <div className='GDbottom-left-container'>
+        <div className='mainGroupDetails'>
+            <div className='groupDetailsTop'>
+                <div className='leftGroupDetailPhotos'>
+                    <img id='GDphoto' src={singleGroup.groupImages?.length > 0 ? singleGroup.groupImages[0].url : ""} />
+
+                </div>
+
+                <div className='rightTopGroupDetails'>
+                    <h2 className='GDname'>{singleGroup.name}</h2>
+                    <h3 className='GDlocation'> {singleGroup.city}, {singleGroup.state}</h3>
+                    <div className='GDmembers-public'> {singleGroup.numMembers} {singleGroup.numMembers === 1 ? "member" : "members"} · {singleGroup.private ? "Private" : "Public"} group </div>
+                    <div className='GDorganizer'> Organized by {singleGroup.organizer.firstName}</div>
+                    <div className='GDbottom-left-container'>
                         <div className='GDabout'>
                             <h4>What are we about? </h4>
                             {singleGroup.about}
                         </div>
                     </div>
-                    </div>
-                    
                 </div>
 
+            </div>
 
-                <div className='otherStuffGD'>
-                    <div className='GDname-container'>
-                        {isOwner &&
+
+            <div className='otherStuffGD'>
+                <div className='GDname-container'>
+                    {isOwner &&
                         <p>Welcome Mr. / Mrs. {singleGroup.organizer.lastName}.  As the organizer of the group how do you wish to proceed?</p>}
-                    </div>
-                
+                </div>
+
                 <div className='GDbuttonsuperDiv'>
                     <div className='GDbutton-container'>
                         {isOwner &&
@@ -93,18 +93,18 @@ export default function GroupDetails(){
                             <button className='GDbutton' onClick={makeEvent}>Create Event</button>}
                     </div>
                 </div>
-                    <div className='GDnotice'>
-                        {!isOwner &&
+                <div className='GDnotice'>
+                    {!isOwner &&
                         <>
                             <p className='GDnoUser'>You are not an organizer of this group.</p>
                             <p className='GDnoUser'>If you are the organizer and do not have access to edit or delete the group <a href='https://help.meetup.com/hc/en-us'>please contact us </a></p>
-                            </>
-                        }
-                    </div>
-
-
+                        </>
+                    }
                 </div>
+
+
+            </div>
         </div>
-        
+
     )
 }
