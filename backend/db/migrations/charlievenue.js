8427e160c8d17f7b56b671bb9b81d7e91d381be6
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-12-05 21:09:08 -0500

    following template

--- backend/db/migrations/charlievenue.js
+++ backend/db/migrations/charlievenue.js
@@ -1,4 +1,25 @@
+
 'use strict';
+
+// NEW: add this code to each create table migration file
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+// END of new code
+
+
+// add options object to up and down functions:
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('table-name', {
+        // ...
+    }, options);    // add options object here
+  },
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('table-name', options); // and here
+  }
+};
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Venues', {
